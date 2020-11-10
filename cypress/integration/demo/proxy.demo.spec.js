0a854a0b94013ad45451d2391a5c1560627860a4
xxxily <974278171@qq.com>
2020-11-10 17:32:18 +0800

    feat:增加测试代码

---
+++ cypress/integration/demo/proxy.demo.spec.js
@@ -0,0 +1,29 @@
+import {proxyHandler, proxyHandlerGenerator} from '../../../src/demo/proxy.demo'
+
+
+describe('proxy.demo单测', () => {
+  before(() => {
+    console.log('-------- Before Test --------')
+  })
+  beforeEach(() => {
+    window._debugMode_ = true
+  })
+  afterEach(() => {
+    cy.clearCookies()
+  })
+
+  it('基本调用', () => {
+    var win = new Proxy(window, proxyHandler)
+    var win2 = new Proxy(window, proxyHandlerGenerator())
+    // expect(win.alert).to.not.equal(win2.alert)
+
+    var xhr = new Proxy(XMLHttpRequest, proxyHandlerGenerator())
+
+    var xhr = new xhr()
+
+    // win.alert(111)
+    // win2.console.log(222)
+    // win2.bb = 111
+    // delete win2.bb
+  })
+})
---
+++ src/libs/hookJs/ajaxHook.js
@@ -0,0 +1,12 @@
+/*!
+ * @name         ajaxHook.js
+ * @description  XMLHttpRequest hook
+ * @version      0.0.1
+ * @author       Blaze
+ * @date         2020/11/10 10:10
+ * @github       https://github.com/xxxily
+ */
+
+const ajaxHook = {}
+
+export default ajaxHook
